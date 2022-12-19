e9880c64b1fd6e13465075a4a44a48fe1ffef3bc
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-12-19 15:18:15 -0700

    files set up

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
+const logs = ["day0", "day1", "day2"];
+
 //Index
 app.get("/logs", (req, res) => {
-  res.send("new");
+  res.send(logs);
 });
+
 //Show
+app.get("/logs/:id", (req, res) => {
+  res.send(logs[req.params.id]);
+});
 
 //New
 
