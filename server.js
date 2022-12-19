f1c86b4020144b80537e92f0438a300b1f61f742
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-12-19 14:49:34 -0700

    express-react-views react@16 react-dom@16

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const app = express();
+
+//Index
+app.get("/logs", (req, res) => {
+  res.send("new");
+});
+//Show
+
+//New
+
+//Create
+
+//Edit
+
+//Update
+
+//Delete
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
