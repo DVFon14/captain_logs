907c2fc612c3bbb4af1425e08ea11d0c90ac8dfd
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-12-19 17:02:29 -0700

    npm install body-parser

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyPaser = require("body-parser");
 
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine()); //similar to requiring
 
+//========Connection to Database========//
+//==============Middleware==============//
+//================ROUTES================//
 //Index
 
 //Show
@@ -14,6 +18,9 @@ app.get("/logs/new", (req, res) => {
 });
 
 //Create
+app.post("/logs", (req, res) => {
+  res.send("received");
+});
 
 //Edit
 
