8c70db983ff5d49e00623f66c5c8bb7367513f6b
Dulce Fonseca <dulcefonseca@Dulces-MacBook-Pro.local>
2022-12-19 17:25:45 -0700

    npm i dotenv

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express();
 const bodyPaser = require("body-parser");
+const mongoose = require("mongoose");
+const Logs = require("./models/logs");
+const methodOverride = require("method-override");
 
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine()); //similar to requiring
 
+app.use(express.urlencoded({ extend: false }));
+app.use(methodOverride("_method"));
+
 //========Connection to Database========//
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
+
 //==============Middleware==============//
+
 //================ROUTES================//
 //Index
 
@@ -19,7 +42,9 @@ app.get("/logs/new", (req, res) => {
 
 //Create
 app.post("/logs", (req, res) => {
-  res.send("received");
+  Logs.create(req.body, (error, newLog) => {
+    res.redirect("/logs");
+  });
 });
 
 //Edit
